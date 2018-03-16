fbf68a3b6fe7eb696a08661d2c141f74c9e8bcd0
llxxll12345 <617415529@qq.com>
2018-03-16 10:33:20 +0800

    Initial GUI

--- src/mainFrame.java
+++ src/mainFrame.java
@@ -1,57 +1,86 @@
 import java.awt.Container;
 import java.awt.Insets;
 import java.awt.Dimension;
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.*;
 
-public class teaching extends JFrame{
+public class mainFrame extends JFrame{
     public static void addComponentsToPane(Container pane) {
         pane.setLayout(null);
 
-        JButton b1 = new JButton("one");
-        JButton b2 = new JButton("two");
-        JButton b3 = new JButton("three");
+        JLabel mainReminder = new JLabel("Please input the expression and variable list");
+        JTextField exprField = new JTextField("INPUT EXPRESSION HERE");
+        JButton btnCalc = new JButton("Do Calculation");
+        JTextField ValueFiled = new JTextField("INPUT Variables and Values here");
+        JButton btnImport = new JButton("Import data from .json");
+        JLabel fileLabel= new JLabel("file name ...");
+        JTextPane resultField = new JTextPane();
 
-        pane.add(b1);
-        pane.add(b2);
-        pane.add(b3);
+        pane.add(btnCalc);
+        pane.add(ValueFiled);
+        pane.add(exprField);
+        pane.add(mainReminder);
+        pane.add(resultField);
+        pane.add(btnImport);
+        pane.add(fileLabel);
 
         Insets insets = pane.getInsets();
-        Dimension size = b1.getPreferredSize();
-        b1.setBounds(25 + insets.left, 5 + insets.top,
-                size.width, size.height);
-        size = b2.getPreferredSize();
-        b2.setBounds(55 + insets.left, 40 + insets.top,
-                size.width, size.height);
-        size = b3.getPreferredSize();
-        b3.setBounds(150 + insets.left, 15 + insets.top,
-                size.width + 50, size.height + 20);
+        setUp(mainReminder, insets, 10, 10);
+        setUp(exprField, insets, 10,40);
+        setUp(ValueFiled, insets, 10, 70);
+        setUp(btnCalc, insets, 10, 100);
+        setUp(resultField, insets, 10, 130, 400, 400);
+
+        resultField.setEditable(false);
+
+        btnCalc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String expr = exprField.getText();
+                Map<String, Double> variables = new HashMap<>();
+                variables.put("x", 5.0);
+                variables.put("y", 5.0);
+                parser myCal = new parser(expr, variables);
+                ExpNode myTree = myCal.parseTree();
+                System.out.println(myCal.deparse(myTree));
+
+                String res = myCal.getResults(myTree);
+                resultField.setText(res);
+                System.out.println(res);
+            }
+        });
     }
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
+
+
+    private static void setUp(JComponent component, Insets insets, int x, int y) {
+        Dimension size = component.getPreferredSize();
+        component.setBounds(insets.left + x, insets.top + y, size.width, size.height);
+    }
+
+    private static void setUp(JComponent component, Insets insets, int x, int y, int incw, int inch) {
+        Dimension size = component.getPreferredSize();
+        component.setBounds(insets.left + x, insets.top + y, size.width + incw, size.height + inch);
+    }
+
     private static void createAndShowGUI() {
-        //Create and set up the window.
-        teaching frame = new teaching();
+        mainFrame frame = new mainFrame();
         frame.setTitle("Demo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Set up the content pane.
         addComponentsToPane(frame.getContentPane());
 
-        //Size and display the window.
         Insets insets = frame.getInsets();
-        frame.setSize(300 + insets.left + insets.right,
-                125 + insets.top + insets.bottom);
+        frame.setSize(500 + insets.left + insets.right,
+                500 + insets.top + insets.bottom);
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
